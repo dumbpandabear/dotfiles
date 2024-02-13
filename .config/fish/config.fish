8668a4c0c3c059ccb534dd185258edcb21773bac
EricLi-Dev <johnlee888123456@gmail.com>
2024-02-13 15:57:30 -0500

    updated alacritty to Nord

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,12 @@ end
 
 starship init fish | source
 zoxide init fish | source
+#source /Users/ericli/opt/anaconda3/etc/fish/conf.d/conda.fish
+source /Users/ericli/anaconda3/etc/fish/conf.d/conda.fish
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /Users/ericli/anaconda3/bin/conda
+    eval /Users/ericli/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
