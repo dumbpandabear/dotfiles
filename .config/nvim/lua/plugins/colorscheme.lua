d71cb31c6a62ea0e48d9c99fda5d9b9fcaf85590
Eric Li <johnlee888123456@gmail.com>
2025-01-21 16:39:20 -0500

    removes all of LazyVim from nvim configs | removes old alacritty yml config

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,19 @@
 -- Manage theme/colorscheme
 -- Install + Change colorscheme
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-
   -- add tokyonight
   {
    "folke/tokyonight.nvim",
-   lazy = true,
    opts = { style = "moon" },
   },
 
   -- add catppuccin
   {
 	  "catppuccin/nvim",
-	  lazy = true,
 	  name = "catppuccin",
-  },
-
-  -- Configure LazyVim to load colorscheme
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
+	  priority = 1000,
+	  init = function()
+		   vim.cmd.colorscheme("catppuccin")
+		end,
   },
 }
